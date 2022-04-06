bf36bae8a0931f46cf1111c39d7a849138b0d4ad
RevelcoS <nertv04@mail.ru>
2022-04-06 16:03:33 +0300

    Simple algorithm to calculate stock prices for each round

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
+from app.stocks import stock
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/stocks")
+def stocks():
+    stock.refresh_prices()
+    return stock.read_table()
